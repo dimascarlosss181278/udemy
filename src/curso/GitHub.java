357db583675f8fe2c1e091488350acad1a79024c
dimascarlosss181278 <dimascarlosss@gmail.com>
2018-10-02 15:49:24 -0300

    Projeto Udemy Criado

--- src/curso/GitHub.java
+++ src/curso/GitHub.java
@@ -6,11 +6,13 @@ public class GitHub {
 
 	public static void main(String[] args) {
 		// Exemplo capitulo GitHub
+
+		System.out.println("INICIANDO...");
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Informe seu nome completo: ");
 		String msg = sc.nextLine(); 
 		System.out.println(msg);
-		
 		
 		sc.close();
 	}
